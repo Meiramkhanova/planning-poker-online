2a1242dc21f23eb703c547f27d7b72214e519356
adinamei <adinaone04@gmail.com>
2026-04-17 13:20:56 +0500

    feat: store

--- src/entities/session/model/store.ts
+++ src/entities/session/model/store.ts
@@ -9,39 +9,56 @@ interface sessionStore {
   register: (credentials: RegisterCredentials) => Promise<void>;
   logout: () => void;
   checkAuth: () => Promise<void>;
+  isLoading: boolean;
+  isProcessing: boolean;
+  error: string | null;
 }
 
 export const useSessionStore = create<sessionStore>((set) => ({
   user: null,
   isAuthenticated: false,
+  isLoading: true,
+  isProcessing: false,
+  error: null,
   login: async (data: LoginCredentials) => {
+    set({ isProcessing: true, error: null });
+
     try {
       const response = await loginRequest(data);
       const { access_token, user } = response;
+
       localStorage.setItem("accessToken", access_token);
 
       set({
         user: user,
         isAuthenticated: true,
       });
-    } catch (err) {
-      console.error(`Ошибка ${err}`);
+    } catch (err: any) {
+      const message = err.response?.data?.message || "Error in Login";
+      set({ error: message });
       throw err;
+    } finally {
+      set({ isProcessing: false });
     }
   },
   register: async (data: RegisterCredentials) => {
+    set({ isProcessing: true, error: null });
     try {
       const response = await registerRequest(data);
       const { access_token, user } = response;
+
       localStorage.setItem("accessToken", access_token);
 
       set({
         user: user,
         isAuthenticated: true,
       });
-    } catch (err) {
-      console.error(`Ошибка ${err}`);
+    } catch (err: any) {
+      const message = err.response?.data?.message || "Error in register";
+      set({ error: message });
       throw err;
+    } finally {
+      set({ isProcessing: false });
     }
   },
   logout: () => {
@@ -50,11 +67,22 @@ export const useSessionStore = create<sessionStore>((set) => ({
     set({
       user: null,
       isAuthenticated: false,
+      error: null,
     });
   },
   checkAuth: async () => {
+    set({ isLoading: true });
+
     try {
+      const token = localStorage.getItem("accessToken");
+
+      if (!token) {
+        set({ isAuthenticated: false, user: null });
+        return;
+      }
+
       const response = await getMe();
+
       set({
         user: response,
         isAuthenticated: true,
@@ -66,6 +94,8 @@ export const useSessionStore = create<sessionStore>((set) => ({
       set({ user: null, isAuthenticated: false });
 
       throw error;
+    } finally {
+      set({ isLoading: false });
     }
   },
 }));
