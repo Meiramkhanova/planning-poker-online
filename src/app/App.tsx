ef45d6394b60c6028e8254263901619df5cd8ae3
adinamei <adinaone04@gmail.com>
2026-04-17 13:35:31 +0500

    added checking and loading initial into app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { useSessionStore } from "@/entities/session/model/store";
+import { useEffect } from "react";
 
 function App() {
+  const checkAuth = useSessionStore((state) => state.checkAuth);
+  const isLoading = useSessionStore((state) => state.isLoading);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading of page</div>;
+  }
+
   return <RouterProvider router={router} />;
 }
 
