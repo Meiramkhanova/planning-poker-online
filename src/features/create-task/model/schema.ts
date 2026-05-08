976dac4d48d38369ba1232c22bc77e84f02592d4
adinamei <adinaone04@gmail.com>
2026-05-08 18:34:44 +0500

    fix: smt

--- src/features/create-task/model/schema.ts
+++ src/features/create-task/model/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createTaskSchema = z.object({
-  title: z.string().min(2, "Add minimum 2 symbols").max(50, "Too long"),
+  title: z.string().min(2, "Add minimum 2 characters").max(50, "Too long"),
   description: z.string().max(100, "No more than 100 symbols").optional(),
   position: z.number().min(0),
 });
